94fdcd195aa396722890cd56c901897768412f40
habib5634 <hero5276311@gmail.com>
2023-10-16 13:33:17 +0500

    scrollbar updateed

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -5,6 +5,7 @@ import Navbar from '../AdminNav/Navbar'
 import {FaBars,FaUsers} from 'react-icons/fa'
 import {RxDashboard} from 'react-icons/rx'
 import {HiUsers} from 'react-icons/hi'
+import './Sidebar.css'
 import {AiOutlineRight,AiOutlineDown,AiOutlineWechat,AiOutlineDollarCircle} from 'react-icons/ai'
 import {MdOutlineWatchLater} from 'react-icons/md'
 import {BiBookContent,BiBell} from 'react-icons/bi'
@@ -136,7 +137,7 @@ const Sidebar = () => {
   return (
     <div className={`h-screen flex overflow-y-auto`}>
       {isOpen && (
-        <div className={`w-1/6 overflow-y-auto bg-white border-r p-3 border-gray-200 `}>
+        <div className={`w-1/6 overflow-y-auto scrollbar bg-white border-r p-3 border-gray-200 `}>
           <div className=" bg-gray-50 h-full">
             
             <div className="flex items-center  h-12">
